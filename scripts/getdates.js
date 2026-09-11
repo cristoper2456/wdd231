efddf09657b345e2c05bbb488e64373caea73594
cristoper2456 <cristophercastrofer@gmail.com>
2026-09-11 00:04:34 -0400

    advanzing I fguess

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+let currentDate = new Date();
+let year = currentDate.getFullYear();
+document.getElementById("currentyear").innerHTML = year;
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
